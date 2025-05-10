54a090d4fb38229970214525b6941f0ed9cc482b
Dhkrmynti <dhikakr3@gmail.com>
2025-05-10 23:52:59 +0700

    testing lagi kontol

--- application/config/database.php
+++ application/config/database.php
@@ -74,10 +74,10 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn' => '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => 'root',
+	'password' => 'dhikamysqlserver',
 	'database' => 'project-web-mahar',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
--- application/controllers/CAdmin.php
+++ application/controllers/CAdmin.php
@@ -4,8 +4,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class CAdmin extends CI_Controller
 {
 
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('MAdmin');
+    }
+
     public function index()
     {
         $this->load->view('welcome_message');
+    }
+
+    public function buat_kategori()
+    {
+        $data['nama'] = $this->input->post('nama');
+
+        $this->MAdmin->tambah('kategori', $data);
+        $this->session->set_flashdata('message', '<div class="alert alert-success">Record has been saved successfully.</div>');
+        redirect(base_url());
     }
 }
