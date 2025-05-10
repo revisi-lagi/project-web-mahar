f11832b589af9e5b838ec96290dd7fcbdc0742db
agngprryg <jendraljerven6@gmail.com>
2025-05-10 23:20:34 +0700

    config : konfigurasi awal kontol

---
+++ application/controllers/CCustomer.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class CCustomer extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('welcome_message');
+    }
+}
---
+++ application/models/MAdmin.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class MAdmin extends CI_Model
+{
+    //
+}
